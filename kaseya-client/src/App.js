238f4682ef1a55b66597bd37be4b065a040ff140
Abbygail Pollack <abbypollack00@gmail.com>
2023-11-16 11:33:49 -0500

    initial folder structure

---
+++ kaseya-client/src/App.js
@@ -0,0 +1,15 @@
+import './App.scss';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
